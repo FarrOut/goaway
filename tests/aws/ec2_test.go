59992f7f6166d2755039c1cb8fa4020f6269daab
Greg Farr <farr.gs@gmail.com>
2018-07-05 10:17:07 +0200

    commented out ec2_test

--- tests/aws/ec2_test.go
+++ tests/aws/ec2_test.go
@@ -8,10 +8,10 @@ import (
 
 func TestListInstances(t *testing.T) {
 
-sess := auth.CreateSession()
-
-	// Create a EC2 client from just a session.
-	svc := ec2.New(sess)
-
+// sess := auth.CreateSession()
+//
+// 	// Create a EC2 client from just a session.
+// 	svc := ec2.New(sess)
+//
 
 }
